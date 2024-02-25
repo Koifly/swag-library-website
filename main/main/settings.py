f4f8d98ff0bd619fbc7124c067f6d8e36d4a59a4
Boop <boop@pop-os.localdomain>
2024-02-25 19:36:00 +0000

    Added dev test env plus fixed my books button

--- main/main/settings.py
+++ main/main/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tauthereal.pythonanywhere.com']
+# DEV
+ALLOWED_HOSTS = ['*']
+
+# PROD
+# ALLOWED_HOSTS = ['tauthereal.pythonanywhere.com']
 
 
 # Application definition
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
+    'sslserver',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
